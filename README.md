22f3d0099c72f79dc1fa76abbda03805da0ce9cf
AliHumadi2000 <aliaaaqh135@gmail.com>
2022-08-09 15:06:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Multi-publishers-and-one-subscriber-using-rabbitmq" 
---
+++ publisher2.py
@@ -0,0 +1,15 @@
+import pika
+from pika.exchange_type import ExchangeType
+# create connection
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost')
+)
+channel = connection.channel()
+# declare the name of the queue
+channel.exchange_declare(exchange='multi', exchange_type=ExchangeType.fanout)
+# declare the name of the queue
+
+message = 'hello world! from publisher 2'
+channel.basic_publish(exchange='multi', routing_key='', body=message)
+print("[x] Sent...",message)
+connection.close()
